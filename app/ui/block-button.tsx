c5f1264f4251439c9a8239baf493056e87eb1ac0
Alper Aygördüler <alperaygorduler61@gmail.com>
2025-04-28 20:48:05 +0300

    Update block-button.tsx

--- app/ui/block-button.tsx
+++ app/ui/block-button.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { MinusCircle } from "lucide-react";
+
 export default function BlockButton() {
-    function handleClick() {
-  
-    }
-    return (
-      <Button variant='outline' onClick={handleClick} className=
-      'ml-1 hover:bg-red-100 border-2 rounded-xl bg-red-50 border-red-300 p-1'>
-        {/* TODO Check Blocked */}
-        <MinusCircle className='w-4 text-red-400'/> 
-      </Button>
-    )
+  function handleClick() {
+    // Block işlemine yönelik mantık (henüz implement edilmedi)
   }
+
+  return (
+    <Button
+      variant="outline"
+      onClick={handleClick}
+      className="ml-1 p-2 border-1 rounded-md text-red-600 border-red-600 hover:bg-red-100 hover:text-red-700 transition-all duration-200 ease-in-out"
+    >
+      <MinusCircle className="w-5 text-red-600 hover:text-red-700" />
+    </Button>
+  );
+}
