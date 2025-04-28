14fcd26561c191b50e7e659c9eec8b13c60759a1
Alper Aygördüler <alperaygorduler61@gmail.com>
2025-04-28 20:46:50 +0300

    Update add-post page.tsx

--- app/add-post/page.tsx
+++ app/add-post/page.tsx
@@ -2,8 +2,13 @@ import PostForm from "@/app/ui/add-post";
 
 export default function AddPostPage() {
   return (
-    <div className="max-w-xl mx-auto mt-10">
-      <h1 className="text-2xl font-bold mb-4">Yeni Gönderi Oluştur</h1>
+    <div className="max-w-2xl mx-auto mt-12 bg-white shadow-xl rounded-xl px-6 py-8">
+      <h1 className="text-3xl font-extrabold text-blue-600 mb-6">
+        Yeni Gönderi Oluştur
+      </h1>
+      <p className="text-lg text-gray-600 mb-6">
+        Paylaşmak istediğiniz içeriği buradan ekleyebilirsiniz.
+      </p>
       <PostForm />
     </div>
   );
