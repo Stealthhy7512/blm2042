1aaa6af3a6f3fd3b5a3c1d3959fb7d3257211f8e
Alper Aygördüler <alperaygorduler61@gmail.com>
2025-04-28 20:50:06 +0300

    Update comment-button.tsx

--- app/ui/comment-button.tsx
+++ app/ui/comment-button.tsx
@@ -1,11 +1,16 @@
-import {ChatBubbleLeftRightIcon} from "@heroicons/react/24/solid";
-import {Toggle} from "@/components/ui/toggle";
-import {Button} from "@/components/ui/button";
+'use client'
 
-export default function CommentButton({comments}: {comments: number}) {
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import { Button } from "@/components/ui/button";
+
+export default function CommentButton({ comments }: { comments: number }) {
   return (
-  <Button variant='ghost' className="text-base text-muted-foreground flex gap-1">
-    <ChatBubbleLeftRightIcon className="w-5" /> {comments}
-  </Button>
+    <Button
+      variant="ghost"
+      className="flex items-center gap-2 text-sm text-muted-foreground hover:text-blue-600 transition-colors duration-200"
+    >
+      <ChatBubbleLeftRightIcon className="w-5" />
+      {comments}
+    </Button>
   );
 }
