118d125813351dcace172b9806f952a3e428e5d2
Alper Aygördüler <alperaygorduler61@gmail.com>
2025-04-28 20:54:17 +0300

    Update id page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,55 +1,53 @@
 import ProfileHeader from "@/components/ui/profileHeader";
-import { PostCard } from '@/app/ui/post-card'
-import { postCard } from '@/app/lib/definitions'
+import { PostCard } from '@/app/ui/post-card';
+import { postCard } from '@/app/lib/definitions';
 
 interface User {
   displayName: string;
   username: string;
   postNumber: number;
-  following: number
-  followers:number
+  following: number;
+  followers: number;
   profilePic: string;
-  banner: string
+  banner: string;
 }
-// FIXME: takes id as parameter then fetches user info with that id
-// props: { params: { id: string } }
-export default async function Home() {
+
+export default function Home(props: { params: { id: string } }) {
+  // Giriş yapmış kullanıcının bilgileri
+  const user: User = {
+    displayName: "Ahmet",
+    username: "ahmet012",
+    postNumber: 12,
+    following: 120,
+    followers: 500,
+    profilePic: "profile.jpg",
+    banner: "banner.jpg",
+  };
+
+  // Örnek gönderi verisi
+  const exPost: postCard = {
+    id: '1',
+    post_owner: 'Kaan Yazici',
+    owner_url: '/customers/evil-rabbit.png',
+    image_url: '/graph.png',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget enim mi...',
+    date: '03/03/2025',
+    likes: 10,
+    comments: 1,
+  };
+
   return (
     <div className="bg-gray-100 min-w-screen min-h-screen p-4">
       <div className="mx-auto">
-
+        {/* Kullanıcı profilini başlık bileşenine gönder */}
         <ProfileHeader user={user} />
-      
+
         <div className="flex items-center flex-col gap-y-[64px] justify-center text-black mt-4">
-              <PostCard Post={exPost}/>
-              <PostCard Post={exPost}/>
+          {/* Gönderi kartlarını render et */}
+          <PostCard Post={exPost} />
+          <PostCard Post={exPost} />
         </div>
       </div>
     </div>
   );
 }
-
-const exPost: postCard = {
-  id: '1',
-  post_owner: 'Kaan Yazici',
-  owner_url: '/customers/evil-rabbit.png',
-  image_url: '/graph.png',
-  message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-    Proin eget enim mi. Duis varius leo augue, et maximus nisl venenatis vitae. \
-    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\
-    Vestibulum ut urna nec nibh tempus varius. Sed auctor varius eros, sed facilisis ipsum interdum porta.\
-    hellllllllllllllllllllllllllllllllllllllloooooooooooooooooooooooooooooo',
-  date: '03/03/2025',
-  likes: 10,
-  comments: 1
-};
-
-const user: User = {
-  displayName: "Ahmet",
-  username: "ahmet012",
-  postNumber: 12,
-  following: 120,
-  followers: 500,
-  profilePic: "profile.jpg",
-  banner: "banner.jpg",
-};
