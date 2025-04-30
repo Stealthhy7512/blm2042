5f36286ebe03053ec8e26af9ffa278fee8ed20c7
Alper Aygördüler <alperaygorduler61@gmail.com>
2025-04-30 12:02:18 +0300

    Update layout.tsx
    Alert message component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header"
+import { Toaster } from 'sonner'  
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Header isAuthenticated={isAuthenticated} />
         <main>{children}</main>
+        <Toaster richColors position="top-right" />
       </body>
       
     </html>
