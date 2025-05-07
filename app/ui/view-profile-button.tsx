dd4c8e04e65b94eaa5ab41752912203e5fcdd2b6
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-05-08 00:03:26 +0300

    Implement View Profile button functionality.

---
+++ app/ui/view-profile-button.tsx
@@ -0,0 +1,26 @@
+import { User } from "lucide-react";
+import { useRouter } from 'next/navigation';
+
+export default function ViewProfile() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    fetch('http://localhost:8080/user/me', {
+      method: 'GET',
+      credentials: 'include',
+    }).then(async res => {
+      const data = await res.json();
+      const username = data.username;
+      if (res.ok) {
+        router.push(`/user/${username}`);
+      }
+    });
+  }
+
+  return (
+    <button onClick={handleClick} className="px-4 py-2 hover:bg-gray-100 flex items-center">
+      <User className="w-4 h-4 mr-2" />View Profile
+    </button>
+  );
+}
+
