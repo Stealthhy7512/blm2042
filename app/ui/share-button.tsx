39e650f426de02d62604f7078aa7dd903f8177ad
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-05-09 01:26:47 +0300

    Share button UI enhancement.

--- app/ui/share-button.tsx
+++ app/ui/share-button.tsx
@@ -3,20 +3,51 @@
 import { ShareIcon } from "@heroicons/react/24/solid";
 import { Button } from "@/components/ui/button";
 import { toast } from 'sonner';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Copy } from 'lucide-react';
 
 export default function ShareButton({ postId }: { postId: number }) {
   const base = 'http://localhost:3000/post/';
 
   return (
-    <Button
-      variant="ghost"
-      className="flex items-center gap-2 text-sm text-muted-foreground hover:text-green-500 hover:scale-110 transition-colors duration-200"
-      onClick={() => {
-        navigator.clipboard.writeText(base+postId)
-          .then(() => toast.success('Copied to clipboard'));
-      } }
-    >
-      <ShareIcon className="w-5" />
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="flex items-center gap-2 text-sm text-muted-foreground hover:text-green-500 hover:scale-110 transition-colors duration-200"
+          onClick={() => {
+            navigator.clipboard.writeText(base+postId)
+              .then(() => toast.success('Copied to clipboard'));
+          } }
+        >
+          <ShareIcon className="w-5" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-auto">
+        <DropdownMenuLabel>Share Link</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <div className="flex flex-row gap-2 mr-2 my-2 items-center">
+          <span className="text-muted-foreground text-sm mx-2 bg-gray-100 rounded-sm px-1.5 py-1.5">
+            {base + postId}
+          </span>
+          <Button
+            variant="ghost"
+            className="transition-colors duration-300 ease-in-out w-5 h-5 p-2 hover:scale-105 hover:bg-gray-100"
+            onClick={() => {
+              navigator.clipboard.writeText(base+postId)
+                .then(() => toast.success('Link copied to clipboard'));
+            } }
+          >
+            <Copy className="w-4"/>
+          </Button>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
