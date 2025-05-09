1a8c49ea384a99306076a5a3ee3cfdca15e916e8
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-05-09 19:48:35 +0300

    Implement postPage.

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,27 +1,7 @@
-import { notFound } from "next/navigation";
-import { PostCard } from "@/app/ui/post-card";
-import { postCard } from "@/app/lib/definitions";
+import PostPage from './postPage';
 
 export default async function PostDetailPage(props: { params: Promise<{ id: string }> }) {
     const { id } = await props.params;
-    const post = {
-        owner_username: "kaanyazici",
-        owner_name: "Kaan Yazici",
-        owner_image_url: "/customers/evil-rabbit.png",
-        content_image_url: "/graph.png",
-        message:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget enim mi.",
-        date: "03/03/2025",
-        likes: 10,
-        comments: 1,
-      } as postCard;
-    //  = posts.find((p) => p.owner_username === params.id); // get post
 
-    if (!post) return notFound();
-
-    return (
-    <main className="max-w-3xl mx-auto py-10 px-4">
-        <PostCard Post={post} />
-    </main>
-    );
+    return <PostPage id={id}/>;
 }
---
+++ app/post/[id]/postPage.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import { notFound } from 'next/navigation';
+import { postCard } from '@/app/lib/definitions';
+import { PostCard } from '@/app/ui/post-card';
+
+export default function PostPage({ id }: { id: string }) {
+  const [post, setPost] = useState<postCard>();
+
+  useEffect(() => {
+    fetch(`/api/post/${id}`, {
+      method: 'GET',
+      credentials: 'include'
+    }).then(async res => {
+      const data = await res.json();
+
+      if (res.ok) {
+        const postData = {
+          postId: parseInt(id),
+          owner_username: data.userSummary.username,
+          owner_name: data.userSummary.visibleName,
+          owner_image_url: data.userSummary.profilePic,
+          content_image_url: data.postImage,
+          message: data.content,
+          date: data.createdAt,
+          likes: data.numberOfPostLike,
+          comments: data.numberOfPostComment,
+          isLiked: data.isPostLiked,
+          isFollowed: data.isPostFollowed,
+        } as postCard;
+        setPost(postData);
+
+      } else if (res.status === 404) {
+        return notFound();
+      }
+    })
+  }, []);
+
+  return (
+    <main className="max-w-3xl mx-auto py-10 px-4">
+      {post && (<PostCard Post={post}/>)}
+    </main>
+  )
+}
