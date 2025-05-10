6c772e617c3836467341d27112ff0c6ba2406833
Stealthhy7512 <kaanyazici7512@gmail.com>
2025-05-10 18:38:04 +0300

    Update config files, move README.

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   async rewrites() {
@@ -8,6 +9,13 @@ const nextConfig: NextConfig = {
         destination: 'http://localhost:8080/:path*',
       },
     ];
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "app"), // or "src" if you're using src/
+    };
+    return config;
   },
 };
 
